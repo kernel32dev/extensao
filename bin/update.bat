98e44a29e21f06fc642415685da729b972d4d019
kernel32dev <kernel32dev@gmail.com>
2023-11-25 08:50:22 -0300

    atualizado update.bat

--- bin/update.bat
+++ bin/update.bat
@@ -5,41 +5,53 @@ set "originalFile=extensao.exe"
 set "newFile=extensao-new.exe"
 set "backupFile=extensao-bkp.exe"
 
-:: Download the new file using curl
-curl -L --fail "https://github.com/kernel32dev/extensao/raw/master/bin/extensao.exe" -o "%newFile%"
 
-:: Check if the download was successful
-if not %errorlevel%==0 (
-    echo Error: Failed to download the new file. Script will exit.
-    exit /b 1
-)
+if exist "%originalFile%" (
 
-:: Compare the two files
-fc /b "%originalFile%" "%newFile%" > nul
+    curl -L --fail "https://github.com/kernel32dev/extensao/raw/master/bin/extensao.exe" -o "%newFile%"
 
-if errorlevel 1 (
-    echo Files are different. Stopping "%originalFile%"...
-    
-    :: Call extensao.exe stop
-    call "%originalFile%" stop
-	if errorlevel 1 (
-		echo Failed to stop service, try executing with administrator privileges
-		exit /b 1
-	)
-    
-    :: Move the old file to backup
-    move "%originalFile%" "%backupFile%" > nul
-    
-    :: Move the new file to the original file's location
-    move "%newFile%" "%originalFile%" > nul
-    
-    echo Starting "%originalFile%"...
-    
-    :: Call extensao.exe start
-    call "%originalFile%" start
+    if not %errorlevel%==0 (
+        echo Error: Failed to download the new file. Script will exit.
+        exit /b 1
+    )
+
+    fc /b "%originalFile%" "%newFile%" > nul
+
+    if errorlevel 1 (
+        echo Files are different. Stopping "%originalFile%"...
+        
+        call "%originalFile%" stop
+        if errorlevel 1 (
+            echo Failed to stop service, try executing with administrator privileges
+            exit /b 1
+        )
+        
+        move "%originalFile%" "%backupFile%" > nul
+        
+        move "%newFile%" "%originalFile%" > nul
+        
+        echo Starting "%originalFile%"...
+        
+        call "%originalFile%" start
+        if errorlevel 1 (
+            echo Failed to start service, try executing with administrator privileges
+            exit /b 1
+        )
+    ) else (
+        echo Files are identical. No action needed.
+        del "%newFile%"
+    )
 ) else (
-    echo Files are identical. No action needed.
-	del "%newFile%"
+    curl -L --fail "https://github.com/kernel32dev/extensao/raw/master/bin/extensao.exe" -o "%originalFile%"
+    if not %errorlevel%==0 (
+        echo Error: Failed to download the new file. Script will exit.
+        exit /b 1
+    )
+    call "%originalFile%" install
+    call "%originalFile%" start
+    if errorlevel 1 (
+        echo Failed to start service, try executing with administrator privileges
+        exit /b 1
+    )
 )
-
 endlocal
